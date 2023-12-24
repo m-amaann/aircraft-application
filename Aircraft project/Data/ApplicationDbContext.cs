bdee0bca7cf08295256297037f5552a5fb3992b7
Mohamed Amaan <amaanrc11@gmail.com>
2023-12-24 12:27:18 +0530

    DbContext

--- Aircraft project/Data/ApplicationDbContext.cs
+++ Aircraft project/Data/ApplicationDbContext.cs
@@ -1,20 +1,20 @@
-﻿//using System;
-//using Microsoft.EntityFrameworkCore;
-//using Aircraft_project.Models;
-//using System.Collections.Generic;
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Aircraft_project.Models;
+using System.Collections.Generic;
 
-//namespace Aircraft_project.Data
-//{
-//	public class ApplicationContext : DbContext
-//    {
-//        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-//       : base(options)
-//        {
-//        }
+namespace Aircraft_project.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+       : base(options)
+        {
+        }
 
-//        public DbSet<User> User { get; set; }
-//    }
-//}
+        public DbSet<Users> User { get; set; }
+    }
+}
 
 
 
