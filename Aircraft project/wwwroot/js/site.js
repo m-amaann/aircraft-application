4f73e88ffd92ff08d686953c569542e405593740
Mohamed Amaan <amaanrc11@gmail.com>
2023-12-26 17:19:27 +0530

    carousal

--- Aircraft project/wwwroot/js/site.js
+++ Aircraft project/wwwroot/js/site.js
@@ -21,8 +21,27 @@ $('#registrationForm').on('submit', function (e) {
 });
 
 
+//PRODUCT CARD CAROASAL
 
-//Add to Cart Icon
+let track = document.querySelector('.carousel-track');
+let cards = Array.from(track.children);
+let cardWidth = cards[0].getBoundingClientRect().width + 20; // Including margin
+let currentSlide = 0;
+
+function moveCarousel(direction) {
+    const trackMoveAmount = cardWidth;
+
+    if (direction === 'next') {
+        currentSlide++;
+        if (currentSlide >= cards.length) currentSlide = 0; // Loop back to the start
+    } else if (direction === 'prev') {
+        currentSlide--;
+        if (currentSlide < 0) currentSlide = cards.length - 1; // Loop back to the end
+    }
+
+    track.style.transform = 'translateX(' + (-trackMoveAmount * currentSlide) + 'px)';
+}
+//Add to Cart Icon for product card
 function slideIcon() {
     var icon = document.querySelector('.add-to-cart .cart-icon');
     icon.style.transform = 'translateX(10px)';
@@ -31,6 +50,9 @@ function slideIcon() {
     },
    300); 
 }
+
+
+
 
 
 
