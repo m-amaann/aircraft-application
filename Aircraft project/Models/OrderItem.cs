c4b36cd4ec9470e4a1a06b4e4b5d5675989ebd41
Mohamed Amaan <amaanrc11@gmail.com>
2023-12-27 20:13:31 +0530

    created modal classes

---
+++ Aircraft project/Models/OrderItem.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Aircraft_project.Models
+{
+    public class OrderItem
+    {
+        [Key] 
+        public int OrderItemId { get; set; }
+
+        [ForeignKey("Orders")]
+        public int OrderId { get; set; }
+
+        [ForeignKey("AircraftProduct")]
+        public int AircraftId { get; set; }
+
+        public int Quantity { get; set; }
+
+        public string Price { get; set; }
+
+        public virtual Orders Orders { get; set; }
+        public virtual AircraftProduct AircraftProduct { get; set; }
+
+    }
+}
---
+++ Aircraft project/Models/Orders.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Aircraft_project.Models
+{
+    public class Orders
+    {
+        [Key] 
+        public int OrderId { get; set; }
+
+        [ForeignKey("Users")]
+        public int UserId { get; set; }
+
+        public DateTime OrderDate { get; set; } = DateTime.Now;
+
+        public required string OrderStatus { get; set; }
+
+        public virtual Users Users { get; set; }
+        public virtual ICollection<OrderItem> OrderItems { get; set; }
+
+
+
+    }
+}
